e37514b3a7a7f2bc2f6993bc73e08a57908424c9
ndneighbor <15745278+ndneighbor@users.noreply.github.com>
2019-12-09 23:15:26 -0500

    added resizing, colors, and brushes

--- ofApp.h
+++ ofApp.h
@@ -1,30 +1,34 @@
+
 #pragma once
 
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
 
-	public:
-		void setup();
-		void update();
-		void draw();
+    public:
+        void setup();
+        void update();
+        void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void mouseEntered(int x, int y);
-		void mouseExited(int x, int y);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+        void keyPressed(int key);
+        void keyReleased(int key);
+        void mouseMoved(int x, int y );
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        void mouseEntered(int x, int y);
+        void mouseExited(int x, int y);
+        void windowResized(int w, int h);
+        void dragEvent(ofDragInfo dragInfo);
+        void gotMessage(ofMessage msg);
     
     
-    int current Brush = 'c';
+    int currentBrush = 0;
+    int colorIndex = 0;
     float brushSize = 10;
-		
+    
+    ofColor color = ofColor::white;
+    
     float mouseX = ofGetMouseX();
     float mouseY = ofGetMouseY();
     ofPolyline line;
